007546d1e3a6597dd6cfffc7d86164451c85dbef
Freddy Vulto <fvulto@gmail.com>
2010-05-30 10:22:59 +0200

    Fixed tests for bash-3.0.0 and bash-2.05b.0. It appears that with bash-3.0.0, declaration AND assignment within one statement doesn't work:
       declare -a a=(e1 'e2  e3')  # WRONG, creates array of 3 elements
       declare -p a
    
    The assignment has to be separated from the declaration:
    
       declare -a a
       a=(e1 'e2  e3')  # OK, creates array of 2 elements
       declare -p a

--- test/lib/upvars.sh
+++ test/lib/upvars.sh
@@ -7,7 +7,15 @@ return_two_optional_values() {
 
 # Params $1..$2  (optional) name of variables to return arrays to
 return_two_optional_arrays() {
-    local a=(foo bar) b=(cee dee)
+    local a b
+    a=(foo bar); b=(cee dee)
     [[ "$1" || "$2" ]] && local ${1+"$1"} ${2+"$2"} && \
         upvars ${1+-a${#a[@]} $1 "${a[@]}"} ${2+-a${#b[@]} $2 "${b[@]}"}
+}
+
+# Param $1  Name of variable to return array to
+return_array() {
+    local r
+    r=(e1 e2 "e3  e4" $'e5\ne6')
+    local "$1" && upvars -a${#r[@]} $1 "${r[@]}"
 }
